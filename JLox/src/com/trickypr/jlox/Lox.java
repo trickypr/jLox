b2bd17f15e08c13e2e51aa2f125a691c45cc6c7c
TrickyPR <trickypr>
2020-05-30 18:19:43 +1000

    Finish scanner

--- JLox/src/com/trickypr/jlox/Lox.java
+++ JLox/src/com/trickypr/jlox/Lox.java
@@ -1,10 +1,73 @@
 package com.trickypr.jlox;
 
-public class Lox {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import com.trickypr.jlox.scanner.Scanner;
+import com.trickypr.jlox.scanner.Token;
 
+public class Lox 
+{
+	static boolean hadError = false;
+	
+	public static void main(String[] args) throws IOException 
+	{
+		if (args.length > 1)
+		{
+			System.out.println("Usage: jlox [script]");
+			System.exit(1);
+		} else if (args.length == 1) 
+		{
+			runFile(args[0]);
+		} else
+		{
+			runPrompt();
+		}
 	}
-
+	
+	private static void runFile(String path) throws IOException 
+	{
+		byte[] bytes = Files.readAllBytes(Paths.get(path));
+		run(new String(bytes, Charset.defaultCharset()));
+		
+		if (hadError) System.exit(1);
+	}
+	
+	private static void runPrompt() throws IOException 
+	{
+		InputStreamReader input = new InputStreamReader(System.in);
+		BufferedReader reader = new BufferedReader(input);
+		
+		for (;;)
+		{
+			System.out.print("> ");
+			run(reader.readLine());
+			
+			hadError = false;
+		}
+	}
+	
+	private static void run(String source)
+	{
+		Scanner scanner = new Scanner(source);
+		List<Token> tokens = scanner.scanTokens();
+		
+		for (Token token : tokens) System.out.println(token);
+	}
+	
+	public static void error(int line, String message) 
+	{
+		report(line, "", message);
+	}
+	
+	private static void report(int line, String where, String message)
+	{
+		System.err.println("[line " + line + "] Error" + where + ": " + message);
+		hadError = true;
+	}
 }
---
+++ JLox/src/com/trickypr/jlox/scanner/Scanner.java
@@ -0,0 +1,261 @@
+package com.trickypr.jlox.scanner;
+
+import static com.trickypr.jlox.scanner.TokenType.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.trickypr.jlox.Lox;
+
+public class Scanner 
+{
+	private final String source;
+	private final List<Token> tokens = new ArrayList<>();
+	private int start = 0;
+	private int current = 0;
+	private int line = 1;
+	
+	private static final Map<String, TokenType> keywords;
+	
+	static {
+		keywords = new HashMap<>();
+		
+		keywords.put("and", AND);
+		keywords.put("class", CLASS);
+		keywords.put("else", ELSE);
+		keywords.put("false", FALSE);
+		keywords.put("for", FOR);
+		keywords.put("fun", FUN);
+		keywords.put("if", IF);
+		keywords.put("nil", NIL);
+		keywords.put("or", OR);
+		keywords.put("print", PRINT);
+		keywords.put("return", RETURN);
+		keywords.put("super", SUPER);
+		keywords.put("this", THIS);
+		keywords.put("true", TRUE);
+		keywords.put("var", VAR);
+		keywords.put("while", WHILE);
+	}
+	
+	public Scanner(String source) 
+	{
+		this.source = source;
+	}
+	
+	public List<Token> scanTokens() 
+	{
+		while (!isAtEnd())
+		{
+			// We are at the beginning of the next lexeme
+			start = current;
+			scanToken();
+		}
+		
+		tokens.add(new Token(EOF, "", null, line));
+		return tokens;
+	}
+	
+	private void scanToken()
+	{
+		char c = advance();
+		
+		switch (c)
+		{
+			case '(':
+				addToken(LEFT_PAREN);
+				break;
+
+			case ')':
+				addToken(RIGHT_PAREN);
+				break;
+			
+			case '{':
+				addToken(LEFT_BRACE);
+				break;
+			
+			case '}':
+				addToken(RIGHT_BRACE);
+				break;
+			
+			case ',':
+				addToken(COMMA);
+				break;
+				
+			case '.':
+				addToken(DOT);
+				break;
+				
+			case '-':
+				addToken(MINUS);
+				break;
+				
+			case '+':
+				addToken(PLUS);
+				break;
+				
+			case ';':
+				addToken(SEMICOLON);
+				break;
+			
+			case '*':
+				addToken(STAR);
+				break;
+				
+			case '!':
+				addToken(match('=') ? BANG_EQUAL : BANG);
+				break;
+				
+			case '=':
+				addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+				break;
+				
+			case '<':
+				addToken(match('=') ? LESS_EQUAL : LESS);
+				break;
+				
+			case '>':
+				addToken(match('=') ? GREATER_EQUAL : GREATER);
+				break;
+				
+			case '/':
+				if (match('/')) 
+				{
+					// Handle comments that go to the end of the line
+					while (peek() != '\n' && !isAtEnd()) advance();
+				} else {
+					addToken(SLASH);
+				}
+				break;
+				
+			case ' ':
+			case '\r':
+			case '\t':
+				// Ignore whitespace
+				break;
+				
+			case '\n':
+				line++;
+				break;
+				
+			case '"':
+				string();
+				break;
+				
+			default:
+				if (isDigit(c)) number();
+				else if (isAlpha(c)) identifier();
+				else Lox.error(line, "Unexpected character " + c);
+				break;
+		}
+	}
+	
+	private void identifier()
+	{
+		while (isAlphaNumeric(peek())) advance();
+		
+		// Check if the identifier is a reserved word
+		String text = source.substring(start, current);
+		
+		TokenType type = keywords.get(text);
+		if (type == null) type = IDENTIFIER;
+		addToken(type);
+	}
+	
+	private boolean isAlpha(char c) {
+		return (c >= 'a' && c <= 'z') ||
+			   (c >= 'A' && c <= 'Z') ||
+			   c == '_';
+	}
+	
+	private boolean isAlphaNumeric(char c) {
+		return isAlpha(c) || isDigit(c);
+	}
+	
+	private void string()
+	{
+		while (peek() != '"' && !isAtEnd()) 
+		{
+			if (peek() == '\n') line++;
+			advance();
+		}
+		
+		// Handle unterminated string
+		if (isAtEnd()) {
+			Lox.error(line, "The file ended without a string being ended");
+			return;
+		}
+		
+		// Pass the closing "
+		advance();
+		
+		// Remove the quotes surrounding it
+		String value = source.substring(start + 1, current - 1);
+		addToken(STRING, value);
+	}
+	
+	private void number()
+	{
+		while (isDigit(peek())) advance();
+		
+		// look for a fractional part
+		if (peek() == '.' && isDigit(peekNext()))
+		{
+			// Consume the "."
+			advance();
+			
+			while (isDigit(peek())) advance();
+		}
+		
+		addToken(NUMBER, Double.parseDouble(source.substring(start, current)));
+	}
+	
+	private boolean match(char expected)
+	{
+		if (isAtEnd()) return false;
+		if (source.charAt(current) != expected) return false;
+		
+		current++;
+		return true;
+	}
+	
+	private char peek()
+	{
+		if (isAtEnd()) return '\0';
+		return source.charAt(current);
+	}
+	
+	private char peekNext()
+	{
+		if (current + 1 >= source.length()) return '\0';
+		return source.charAt(current + 1);
+	}
+	
+	private boolean isDigit(char c) {
+		return c >= '0' && c <= '9';
+	}
+	
+	private boolean isAtEnd() 
+	{
+		return current >= source.length();
+	}
+	
+	private char advance() 
+	{
+		current++;
+		return source.charAt(current -1);
+	}
+	
+	private void addToken(TokenType token) 
+	{
+		addToken(token, null);
+	}
+	
+	private void addToken(TokenType type, Object literal)
+	{
+		String text = source.substring(start, current);
+		tokens.add(new Token(type, text, literal, line));
+	}
+}
